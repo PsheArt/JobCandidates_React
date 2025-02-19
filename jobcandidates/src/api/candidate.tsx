031d5293389d7cb903dad71c2065ae24073ff932
Tema <pshenichnykh.art@yandex.com>
2025-02-19 18:41:39 +0800

    Update candidate.tsx

--- jobcandidates/src/api/candidate.tsx
+++ jobcandidates/src/api/candidate.tsx
@@ -2,9 +2,12 @@ import axios from 'axios';
 const API_URL = 'http://localhost:5000/api/candidates';
 
 export const getCandidates = async () => {
-    return await axios.get(API_URL + '/get');
+    return await axios.get(API_URL + '/all');
 };
 
-export const getCandidate = async (id: string) => {;
-    return await axios.get(`${API_URL}/${getCandidate}/${id}`);
+export const getCandidate = async (id: string) => {
+    return await axios.get(`${API_URL}/get_${id}`);
+};
+export const newCandidate= async () => {
+    return await axios.get(`${API_URL}/newcandidate`);
 };
