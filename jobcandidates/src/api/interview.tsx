61708ec06b0ab7f9995f0132381b4580fe646037
Tema <pshenichnykh.art@yandex.com>
2025-02-26 17:30:32 +0800

    Update interview.tsx

--- jobcandidates/src/api/interview.tsx
+++ jobcandidates/src/api/interview.tsx
@@ -2,10 +2,13 @@ import axios from 'axios';
 const API_URL = 'http://localhost:5000/api/interviews';
 
 export const getInterviews = async () => {
-    return await axios.get(API_URL + '/get');
+    return await axios.get(API_URL + '/GetInterviews');
 };
 
 export const getInterview = async (id: string) => {
-    ;
+    
     return await axios.get(`${API_URL}/${getInterview}/${id}`);
 };
+export const getInterviewer = async (id: string) => {
+    return await axios.get(`${API_URL}/${getInterviewer}/${id}`)
+}
